77b44978f2e88c300ab1c0bce4625b7a260eeaaa
Daria2002 <daria.matkovic@fer.hr>
2019-04-05 12:29:46 +0200

    Connect gazebo picture with line detection code.

--- src/scripts/publisher.py
+++ src/scripts/publisher.py
@@ -3,20 +3,33 @@
 import rospy;
 from geometry_msgs.msg import Twist
 
-def commander(v, w):
+def commander(l_x, l_y, l_z, a_x, a_y, a_z):
     while not rospy.is_shutdown():
         vel = Twist();
-        vel.linear.x = v;
-        vel.angular.z = w;
+        vel.linear.x = l_x;
+        vel.linear.y = l_y;
+        vel.linear.z = l_z;
+        
+        vel.angular.x = a_x;
+        vel.angular.y = a_y;
+        vel.angular.z = a_z;
         pub.publish(vel);
         rospy.sleep(1.0);
 
 if __name__ == '__main__':
     pub = rospy.Publisher('cmd_vel',Twist,queue_size=1)
     rospy.init_node('publisher')
-    v = 2; w = 0
+
+    l_x = 2    
+    l_y = 0
+    l_z = 0
+    
+    a_x = 0
+    a_y = 0
+    a_z = 1
+
     try:
         while not rospy.is_shutdown():
-            commander(v,w)
+            commander(l_x, l_y, l_z, a_x, a_y, a_z)
     except rospy.ROSInterruptException:
         pass
--- src/scripts/subscriber.py
+++ src/scripts/subscriber.py
@@ -11,14 +11,15 @@ __version__=  '0.1'
 __license__ = 'BSD'
 # Python libs
 import sys, time
-
+import copy
 # numpy and scipy
 import numpy as np
 from scipy.ndimage import filters
-
+import matplotlib.pyplot as plt
 # OpenCV
 import cv2
 
+import scipy.misc
 # Ros libraries
 import roslib
 import rospy
@@ -38,13 +39,13 @@ class image_feature:
         # subscribed Topic
         self.subscriber = rospy.Subscriber("morus/camera1/image_raw/compressed",
             CompressedImage, self.callback,  queue_size = 1)
-        print('ok')
+        
         if VERBOSE :
             print "subscribed to /camera/image/compressed"
 
 
     def callback(self, ros_data):
-        print('ok je ')
+        
         '''Callback function of subscribed topic. 
         Here images get converted and features detected'''
         if VERBOSE :
@@ -52,15 +53,11 @@ class image_feature:
 
         #### direct conversion to CV2 ####
         np_arr = np.fromstring(ros_data.data, np.uint8)
-        #image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
         image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR) # OpenCV >= 3.0:
         
         #### Feature detectors using CV2 #### 
         # "","Grid","Pyramid" + 
         # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
-        #method = "GridFAST"
-        #method = cv2.ORB_create()
-        #feat_det = cv2.FeatureDetector_create(method)
         feat_det = cv2.ORB_create()
         time1 = time.time()
 
@@ -79,15 +76,93 @@ class image_feature:
         cv2.imshow('cv_img', image_np)
         cv2.waitKey(2)
 
-        #### Create CompressedIamge ####
-        msg = CompressedImage()
-        msg.header.stamp = rospy.Time.now()
-        msg.format = "jpeg"
-        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
-        # Publish new image
-        self.image_pub.publish(msg)
+        inputImage = scipy.misc.toimage(image_np)
+    
         
-        #self.subscriber.unregister()
+        plt.imshow(inputImage)
+
+        # gray_image = cv2.cvtColor(inputImage, cv2.COLOR_BGR2GRAY)
+        # plt.subplot(332)
+        # plt.imshow(gray_image, cmap="gray")
+
+        blur = cv2.medianBlur(image_np, 5)
+        plt.subplot(335)
+        plt.imshow(inputImage, cmap="gray")
+
+        edges = cv2.Canny(blur, 150, 270)
+        plt.subplot(333)
+        plt.imshow(edges, cmap="gray")
+        edges = cv2.dilate(edges, None)
+
+        # TODO: Try different parameters
+        threshold = 60
+        minLineLength = 10
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, 110)
+        line_img, theta_deg, theta_rad = draw_hough_lines(lines, copy.deepcopy(inputImage), image_np)
+        
+        print(theta_deg)
+        #plt.subplot(334)
+        #plt.imshow(line_img)
+
+        #plt.tight_layout()
+        #plt.show()
+
+def get_points(rho, theta):
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a * rho
+        y0 = b * rho
+        x1 = int(x0 + 2000 * (-b))
+        y1 = int(y0 + 2000 * (a))
+        x2 = int(x0 - 2000 * (-b))
+        y2 = int(y0 - 2000 * (a))
+
+        return x1, y1, x2, y2
+
+def draw_hough_lines(lines, img, image_np):
+        """
+        Draw Hough lines on the given image
+        :param lines: Line array.
+        :param img: Given image.
+        :return: Image with drawn lines
+        """
+
+        r_acc = 0
+        theta_acc = 0
+        accepted_count = 0
+
+        for line in lines:
+
+            # Extract line info
+            r = line[0][0]
+            theta = line[0][1]
+
+            x1, y1, x2, y2 = get_points(r, theta)
+            cv2.line(image_np, (x1, y1), (x2, y2), (0, 0, 255), 3)
+            # Check if line is accepted as good
+            # f r > 0 and temp_theta > 0 and temp_theta < 0 or r < 0 and temp_theta < 180 and temp_theta > 150:
+            #print(theta * 180 / np.pi, r)
+            # Check if line is accepted as good
+
+            if -60<abs(abs(theta * 180 / np.pi) - 90) < 20:
+                continue
+
+            accepted_count += 1
+            r_acc += r
+            theta_acc += theta
+
+        # Plot average accepted
+        if accepted_count > 0:
+            theta = theta_acc / accepted_count
+            r = r_acc / accepted_count
+            x1, y1, x2, y2 = get_points(r, theta)
+            cv2.line(image_np, (x1, y1), (x2, y2), (255, 0, 0), 10)
+            
+            #print("Best theta: ", theta * 180 / np.pi)
+            theta_deg = theta * 180 / np.pi
+        #print("/n/n")
+        #theta is in rad
+        return img, theta_deg, theta
 
 def main(args):
     '''Initializes and cleanup ros node'''
